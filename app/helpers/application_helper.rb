613575d65a0ec9dbcfdf4da286de0cec904c51f6
Art <soll.sterben@gmail.com>
2017-11-30 19:15:28 +0000

    Finished layouts and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    def page_title(add_title = '')
+    
+        @base_title = "Ruby on Rails Tutorial Sample App"
+        if add_title == ''
+          @base_title
+        else
+          "#{add_title} | #{@base_title}"
+        end
+    
+    end
 end
